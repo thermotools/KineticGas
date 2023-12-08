2406f5750e38300f20df17355fe22a0830b80993
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2023-12-08 10:53:59 +0100

    Implemented contribution to thermal conductivity from internal degrees of freedom
    Relation collected from SuperTRAPP documentation, doi.org/10.6028/NIST.IR.8209

--- pykingas/py_KineticGas.py
+++ pykingas/py_KineticGas.py
@@ -595,7 +595,7 @@ class py_KineticGas:
         alpha = self.thermal_diffusion_factor(T, Vm, x, N=N)
         return alpha / T
 
-    def thermal_conductivity(self, T, Vm, x, N=None):
+    def thermal_conductivity(self, T, Vm, x, N=None, idealgas=None, include_internal=False, contributions='all'):
         r"""TV-Property
         Compute the thermal conductivity, $\lambda$. For models initialized with `is_idealgas=True`, the thermal
         conductivity is not a function of density (i.e. $d \lambda / d V_m = 0$).
@@ -606,12 +606,22 @@ class py_KineticGas:
             Vm (float) : Molar volume [m3 / mol]
             x (array_like) : Molar composition [-]
             N (int, optional) : Enskog approximation order (>= 2)
+            idealgas (bool, optional) : Return infinite dilution value? Defaults to model default (set on init).
+            include_internal (bool, optional) : Include contribution from internal degrees of freedom, computed using
+                                                Eucken equation (doi.org/10.6028/NIST.IR.8209). Defaults to False.
+            contributions (str, optional) : Return only specific contributions, can be ('all', '(i)nternal',
+                                            '(t)ranslational', '(d)ensity', or several of the above, such as 'tid' or 'td'.
+                                            If several contributions are selected, these are returned in an array of
+                                            contributions in the same order as indicated in the supplied flag.
 
         Returns:
             (float) : The thermal conductivity of the mixture.
         """
         if N is None:
             N = self.default_N
+        if idealgas is None:
+            idealgas = self.is_idealgas
+
         self.check_valid_composition(x)
         particle_density = Avogadro / Vm
         a = self.compute_cond_vector(particle_density, T, x, N=N)
@@ -620,14 +630,6 @@ class py_KineticGas:
         cd = self.get_contact_diameters(particle_density, T, x)
         d = self.compute_diffusion_coeff_vector(particle_density, T, x, N=N)
         d = self.reshape_diffusion_coeff_vector(d)
-
-        lambda_dblprime = 0
-        if self.is_idealgas is False: # lambda_dblprime is only nonzero when density corrections are present, and vanishes at infinite dilution
-            for i in range(self.ncomps):
-                for j in range(self.ncomps):
-                    lambda_dblprime += particle_density ** 2 * np.sqrt(2 * pi * self.m[i] * self.m[j] * Boltzmann * T / (self.m[i] + self.m[j])) \
-                                        * (x[i] * x[j]) / (self.m[i] + self.m[j]) * (cd[i][j] ** 4) * rdf[i][j]
-            lambda_dblprime *= (4 * Boltzmann / 3)
 
         lambda_prime = 0
         dth = self.compute_dth_vector(particle_density, T, x, N=N)
@@ -638,10 +640,38 @@ class py_KineticGas:
             lambda_prime += x[i] * K[i] * (a[self.ncomps + i] - tmp)
         lambda_prime *= (5 * Boltzmann / 4)
 
-        cond = lambda_prime + lambda_dblprime
-        return cond
+        lambda_int = 0
+        if include_internal is True:
+            f_int = 1.32e3
+            eta_0 = self.viscosity(T, Vm, x, N=N, idealgas=True)
+            Cp_factor = 0
+            for i in range(self.ncomps):
+                _, Cpi_id = self.eos.idealenthalpysingle(T, (1 if self._is_singlecomp else (i + 1)), dhdt=True)
+                Mi = self.mole_weights[i] * Avogadro * 1e3  # Mole weight in g / mol
+                Cp_factor += x[i] * (Cpi_id - 5 * gas_constant / 2) / Mi
 
-    def viscosity(self, T, Vm, x, N=None):
+            lambda_int = f_int * eta_0 * Cp_factor
+
+        lambda_dblprime = 0
+        if idealgas is False:  # lambda_dblprime is only nonzero when density corrections are present, and vanishes at infinite dilution
+            for i in range(self.ncomps):
+                for j in range(self.ncomps):
+                    lambda_dblprime += particle_density ** 2 * np.sqrt(
+                        2 * pi * self.m[i] * self.m[j] * Boltzmann * T / (self.m[i] + self.m[j])) \
+                                       * (x[i] * x[j]) / (self.m[i] + self.m[j]) * (cd[i][j] ** 4) * rdf[i][j]
+            lambda_dblprime *= (4 * Boltzmann / 3)
+
+        cond = lambda_prime + lambda_int + lambda_dblprime
+        if contributions == 'all':
+            return cond
+
+        contribs = {'t' : lambda_prime, 'i' : lambda_int, 'd' : lambda_dblprime}
+        if len(contributions) > 1:
+            return np.array([contribs[c] for c in contributions])
+        else:
+            return contribs[contributions]
+
+    def viscosity(self, T, Vm, x, N=None, idealgas=None):
         r"""TV-Property
         Compute the shear viscosity, $\eta$. For models initialized with `is_idealgas=True`, the shear viscosity
         is not a function of density (i.e. $d \eta / d V_m = 0). See Eq. (12) in RET for Mie fluids (https://doi.org/10.1063/5.0149865)
@@ -651,12 +681,15 @@ class py_KineticGas:
             Vm (float) : Molar volume [m3 / mol]
             x (array_like) : Molar composition [-]
             N (int, optional) : Enskog approximation order
+            idealgas (bool, optional) : Use infinite dilution value? Defaults to model default value (set on init)
 
         Returns:
             (float) : The shear viscosity of the mixture.
         """
         if N is None:
             N = self.default_N
+        if idealgas is None:
+            idealgas = self.is_idealgas
         self.check_valid_composition(x)
 
         particle_density = Avogadro / Vm
@@ -671,7 +704,7 @@ class py_KineticGas:
         eta_prime *= Boltzmann * T / 2
 
         eta_dblprime = 0
-        if self.is_idealgas is False: # eta_dblprime is only nonzero when density corrections are present, and vanish at infinite dilution
+        if idealgas is False: # eta_dblprime is only nonzero when density corrections are present, and vanish at infinite dilution
             for i in range(self.ncomps):
                 for j in range(self.ncomps):
                     eta_dblprime += np.sqrt(self.m[i] * self.m[j] / (self.m[i] + self.m[j])) * x[i] * x[j] * cd[i][j]**4 * rdf[i][j]
