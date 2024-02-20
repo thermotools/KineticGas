0ca8579bfa0c93aa094ee897aadf5b682cc987fe
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-02-20 10:45:18 +0100

    Cleaned up tests for radial distribution function.

--- tests/sutherland/test_rdf.py
+++ tests/sutherland/test_rdf.py
@@ -1,3 +1,6 @@
+"""
+Test module that ensures that the new Sutherland model reproduces the RDF at contact computed using the old MieKinGas model
+"""
 from pykingas.Sutherland import S_MieKinGas
 from pykingas.MieKinGas import MieKinGas
 from scipy.constants import Avogadro, Boltzmann
@@ -11,15 +14,11 @@ T_lst = [100, 500, 1000]
 p_lst = [1e2, 1e6, 5e7]
 
 @pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
 @pytest.mark.parametrize('T', T_lst)
 @pytest.mark.parametrize('p', p_lst)
-def test_mie_g0(comps, i, j, T, p, silent=False):
+def test_mie_g0(comps, T, p, silent=False):
     mie = MieKinGas(comps)
     smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
     
     z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
     z /= sum(z)
@@ -28,23 +27,21 @@ def test_mie_g0(comps, i, j, T, p, silent=False):
     
     g0_mie = mie.cpp_kingas.rdf_g0(rho, T, z)
     g0_suth = smie.cpp_kingas.rdf_g0(rho, T, z)
-    if silent is False:
-        print(f'(i, j) = ({i}, {j})')
-        print(f'g0 (mie) : {g0_mie[i][j]}')
-        print(f'g0 (sut) : {g0_suth[i][j]}')
-        print(f'Diff : {(g0_mie[i][j] - g0_suth[i][j]) / g0_mie[i][j]}')
-    assert equal(g0_mie[i][j], g0_suth[i][j])
+    for i in range(mie.ncomps):
+        for j in range(mie.ncomps):
+            if silent is False:
+                print(f'(i, j) = ({i}, {j})')
+                print(f'g0 (mie) : {g0_mie[i][j]}')
+                print(f'g0 (sut) : {g0_suth[i][j]}')
+                print(f'Diff : {(g0_mie[i][j] - g0_suth[i][j]) / g0_mie[i][j]}')
+            assert equal(g0_mie[i][j], g0_suth[i][j])
 
 @pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
 @pytest.mark.parametrize('T', T_lst)
 @pytest.mark.parametrize('p', p_lst)
-def test_mie_g1(comps, i, j, T, p, silent=False):
+def test_mie_g1(comps, T, p, silent=False):
     mie = MieKinGas(comps)
     smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
     
     z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
     z /= sum(z)
@@ -53,25 +50,23 @@ def test_mie_g1(comps, i, j, T, p, silent=False):
     
     g1_mie = mie.cpp_kingas.rdf_g1(rho, T, z)
     g1_suth = smie.cpp_kingas.rdf_g1(rho, T, z)
-    if silent is False:
-        print(f'(i, j) = ({i}, {j})')
-        print(f'g1 (mie) : {g1_mie[i][j]}')
-        print(f'g1 (sut) : {g1_suth[i][j]}')
-        print(f'Diff : {(g1_mie[i][j] - g1_suth[i][j]) / g1_mie[i][j]}')
-    assert equal(g1_mie[i][j], g1_suth[i][j], tol=1e-8)
+    for i in range(mie.ncomps):
+        for j in range(mie.ncomps):
+            if silent is False:
+                print(f'(i, j) = ({i}, {j})')
+                print(f'g1 (mie) : {g1_mie[i][j]}')
+                print(f'g1 (sut) : {g1_suth[i][j]}')
+                print(f'Diff : {(g1_mie[i][j] - g1_suth[i][j]) / g1_mie[i][j]}')
+            assert equal(g1_mie[i][j], g1_suth[i][j], tol=1e-8)
     print('Test passed\n')
 
 
 @pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
 @pytest.mark.parametrize('T', T_lst)
 @pytest.mark.parametrize('p', p_lst)
-def test_mie_g2(comps, i, j, T, p, silent=False):
+def test_mie_g2(comps, T, p, silent=False):
     mie = MieKinGas(comps)
     smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
 
     z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
     z /= sum(z)
@@ -80,24 +75,22 @@ def test_mie_g2(comps, i, j, T, p, silent=False):
 
     g2_mie = mie.cpp_kingas.rdf_g2(rho, T, z)
     g2_suth = smie.cpp_kingas.rdf_g2(rho, T, z)
-    if silent is False:
-        print(f'(i, j) = ({i}, {j})')
-        print(f'g2 (mie) : {g2_mie[i][j]}')
-        print(f'g2 (sut) : {g2_suth[i][j]}')
-        print(f'Diff : {(g2_mie[i][j] - g2_suth[i][j]) / g2_mie[i][j]}')
-    assert equal(g2_mie[i][j], g2_suth[i][j], tol=1e-1)
+    for i in range(mie.ncomps):
+        for j in range(mie.ncomps):
+            if silent is False:
+                print(f'(i, j) = ({i}, {j})')
+                print(f'g2 (mie) : {g2_mie[i][j]}')
+                print(f'g2 (sut) : {g2_suth[i][j]}')
+                print(f'Diff : {(g2_mie[i][j] - g2_suth[i][j]) / g2_mie[i][j]}')
+            assert equal(g2_mie[i][j], g2_suth[i][j], tol=1e-1)
     print('Test passed\n')
 
 @pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
 @pytest.mark.parametrize('T', T_lst)
 @pytest.mark.parametrize('p', p_lst)
-def test_mie_g(comps, i, j, T, p, silent=False):
+def test_mie_g(comps, T, p, silent=False):
     mie = MieKinGas(comps)
     smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
 
     z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
     z /= sum(z)
@@ -106,82 +99,19 @@ def test_mie_g(comps, i, j, T, p, silent=False):
 
     g_mie = mie.get_rdf(rho, T, z)
     g_suth = smie.get_rdf(rho, T, z)
-    if silent is False:
-        print(f'(i, j) = ({i}, {j})')
-        print(f'g (mie) : {g_mie[i][j]}')
-        print(f'g (sut) : {g_suth[i][j]}')
-        print(f'Diff : {(g_mie[i][j] - g_suth[i][j]) / g_mie[i][j]}')
-    assert equal(g_mie[i][j], g_suth[i][j], tol=1e-1)
+    for i in range(mie.ncomps):
+        for j in range(mie.ncomps):
+            if silent is False:
+                print(f'(i, j) = ({i}, {j})')
+                print(f'g (mie) : {g_mie[i][j]}')
+                print(f'g (sut) : {g_suth[i][j]}')
+                print(f'Diff : {(g_mie[i][j] - g_suth[i][j]) / g_mie[i][j]}')
+            assert equal(g_mie[i][j], g_suth[i][j], tol=1e-10)
     print('Test passed\n')
 
-@pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
-@pytest.mark.parametrize('T', T_lst)
-@pytest.mark.parametrize('p', p_lst)
-def test_mie_d2(comps, i, j, T, p, silent=False):
-    mie = MieKinGas(comps)
-    smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
-    
-    z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
-    z /= sum(z)
-    Vm, = mie.eos.specific_volume(T, p, z, mie.eos.VAPPH)
-    rho = Avogadro / Vm 
-    
-    lambdas = smie.lambdas
-
-    d_BH = mie.cpp_kingas.get_BH_diameters(T)
-    mie_2d = np.array(mie.cpp_kingas.da2_div_chi_drho(rho, T, z))
-    suth_2d = np.array(smie.cpp_kingas.da2_div_chi_drho(rho, T, z))
-    # print(g1_mie)
-    # print(g1_suth)
-    if silent is False:
-        print(T, p / 1e5) # , max(g1_mie.flatten()), max(g1_suth.flatten()))
-        print(f'(i, j) = ({i}, {j})')
-        print(f'Mie  : {mie_2d}')
-        print(f'Suth : {suth_2d}')
-        print(f'Diff : {(mie_2d - suth_2d) / mie_2d}')
-    if mie_2d[i][j] == 0: return
-    assert equal(mie_2d[i][j], suth_2d[i][j])
-
-@pytest.mark.parametrize('comps', complist)
-@pytest.mark.parametrize('i', idx_list)
-@pytest.mark.parametrize('j', idx_list)
-@pytest.mark.parametrize('T', T_lst)
-@pytest.mark.parametrize('p', p_lst)
-def test_mie_d0(comps, i, j, T, p, silent=False):
-    mie = MieKinGas(comps)
-    smie = S_MieKinGas(comps)
-    if (i >= mie.ncomps) or (j >= mie.ncomps):
-        return
-    
-    z = np.array([i + 1 for i in range(mie.ncomps)], dtype=float)
-    z /= sum(z)
-    Vm, = mie.eos.specific_volume(T, p, z, mie.eos.VAPPH)
-    rho = Avogadro / Vm 
-    
-    lambdas = smie.lambdas
-
-    d_BH = mie.cpp_kingas.get_BH_diameters(T)
-    mie_val = mie.cpp_kingas.zeta_x(rho, z, d_BH)
-    suth_val = smie.cpp_kingas.zeta_x(rho, z, d_BH)
-    # print(g1_mie)
-    # print(g1_suth)
-    if silent is False:
-        print(T, p / 1e5) # , max(g1_mie.flatten()), max(g1_suth.flatten()))
-        # print(f'(i, j) = ({i}, {j})')
-        # print(f'g1 (mie) : {g1_mie}')
-        # print(f'g1 (sut) : {g1_suth}')
-        print(f'Diff : {(mie_val - suth_val) / mie_val}')
-    assert equal(mie_val, suth_val)
 
 if __name__ == '__main__':
-    for comps in complist:
-        for T in T_lst:
-            for p in p_lst:
-                for i in idx_list:
-                    for j in idx_list:
-                        test_mie_g2(comps, i, j, T, p, silent=False)
-                        # print('Test passed!\n')
+    for comps_ in complist:
+        for T_ in T_lst:
+            for p_ in p_lst:
+                test_mie_g2(comps_, T_, p_, silent=False)
