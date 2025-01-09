c8249d1e7cc06be65575262c3252b501934c72c5
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2025-01-09 23:25:20 +0100

    Implemented FH_Corrected template class.

--- cpp/extentions.h
+++ cpp/extentions.h
@@ -41,7 +41,6 @@ public:
     }
 
     using T::potential;
-    
     double potential(int i, int j, double r) override {
         if (using_tabulated){
             if (j < i) std::swap(i, j);
@@ -77,4 +76,184 @@ private:
     std::vector<std::vector<FuncTable<N, deg>>> tab_potential;
     std::vector<std::vector<FuncTable<N, deg>>> tab_potential_r;
     std::vector<std::vector<FuncTable<N, deg>>> tab_potential_rr;
+};
+
+
+template<typename T, size_t N, size_t deg>
+class Splined : public T {
+public:
+
+    template<typename... Args>
+    Splined(Args&&... args)
+        : T(std::forward<Args>(args)...), 
+        spl_min{0.5}, spl_max{10.},
+        A_head(T::Ncomps, vector1d(T::Ncomps, 0.)),
+        B_head(T::Ncomps, vector1d(T::Ncomps, 0.)),
+        C_tail(T::Ncomps, vector1d(T::Ncomps, 0.)),
+        spl_potential(T::Ncomps, std::vector(T::Ncomps, FuncSpline<N, deg>()))
+    {
+        update_spline();
+    }
+
+    inline void set_using_spline(bool use_spl){
+        using_spline = use_spl; 
+        update_spline();
+    };
+
+    bool get_spline_is_active(){return using_spline;}
+
+    void set_spline_domain(double start, double stop){
+        spl_min = start, spl_max = stop;
+        update_spline();
+    }
+
+    std::pair<double, double> get_spline_domain(){
+        double dx = deg * (spl_max - spl_min) / N;
+        return std::pair<double, double>(spl_min + dx, spl_max - dx);
+    }
+
+    void update_spline(){
+        for (size_t i = 0; i < T::Ncomps; i++){
+            for (size_t j = 0; j < T::Ncomps; j++){
+                spl_potential[i][j] = FuncSpline<N, deg>([&](double r){return T::potential(i, j, r);}, spl_min * T::sigma[i][j], spl_max * T::sigma[i][j]);
+                C_tail[i][j] = T::potential(i, j, spl_max * T::sigma[i][j]) * pow(spl_max, 6);
+                double u_head = T::potential(i, j, spl_min * T::sigma[i][j]);
+                double ur_head = T::potential_derivative_r(i, j, spl_min * T::sigma[i][j]);
+                B_head[i][j] = - u_head / ur_head;
+                A_head[i][j] = u_head / exp(- B_head[i][j] * spl_min * T::sigma[i][j]);
+            }
+        }
+        
+    }
+
+    size_t set_internals(double rho, double temp, const vector1d& x) override {
+        size_t r = T::set_internals(rho, temp, x);
+        if (using_spline && r) update_spline();
+        return r;
+    }
+
+    double potential_n(int i, int j, double r, int n){
+        if (n < 0) throw std::out_of_range("Cannot compute derivative < 0.");
+        if (using_spline){
+            if (j < i) std::swap(i, j);
+            if (spl_potential[i][j].in_valid_range(r)){
+                return spl_potential[i][j].derivative(r, n);
+            }
+            if (n > 2) {
+                std::pair<double, double> domain = get_spline_domain();
+                if (r >= domain.second * T::sigma[i][j]) {
+                    return pow(-1, n + 1) * C_tail[i][j] * pow(T::sigma[i][j] / r, 6 + n) / pow(T::sigma[i][j], n);
+                }
+                return pow( - B_head[i][j], n) * A_head[i][j] * exp( - B_head[i][j] * r);
+                throw std::out_of_range("Derivative order (" + std::to_string(n) + " > 2) only available in Splined region (" 
+                                        + std::to_string(domain.first) + ", " + std::to_string(domain.second) + "), got " + std::to_string(r / T::sigma[i][j]) );
+            }
+        }
+        switch (n){
+            case 0: return T::potential(i, j, r); 
+            case 1: return T::potential_derivative_r(i, j, r);
+            case 2: return T::potential_dblderivative_rr(i, j, r);
+            default: throw std::out_of_range("Derivative order (" + std::to_string(n) + " > 2) only available when Spline is active (use set_using_spline).");
+        }
+    }
+
+    using T::potential;
+    double potential(int i, int j, double r) override {return potential_n(i, j, r, 0); }
+    double potential_derivative_r(int i, int j, double r) override {return potential_n(i, j, r, 1);}
+    double potential_dblderivative_rr(int i, int j, double r) override {return potential_n(i, j, r, 2);}
+    
+
+private:
+    bool using_spline = true;
+    double spl_min, spl_max;
+    vector2d A_head, B_head, C_tail;
+    std::vector<std::vector<FuncSpline<N, deg>>> spl_potential;
+
+};
+
+template<typename T, size_t FH_order>
+class FH_Corrected : public T {
+public:
+    template<typename... Args>
+    FH_Corrected(Args&&... args)
+        : T(std::forward<Args>(args)...),
+        D_factors(T::Ncomps, vector1d(T::Ncomps))
+    {
+        set_D_factors();
+        T::set_using_spline(true);
+    }
+
+    double potential(int i, int j, double r, double temp){
+        set_internals(0, temp, {0.});
+        return potential(i, j, r);
+    }
+
+    double potential_derivative_r(int i, int j, double r, double temp){
+        set_internals(0, temp, {0.});
+        return potential_derivative_r(i, j, r);
+    }
+
+
+    double potential_dblderivative_rr(int i, int j, double r, double temp){
+        set_internals(0, temp, {0.});
+        return potential_dblderivative_rr(i, j, r);
+    }
+
+    using T::potential;
+    double potential(int i, int j, double r) override {
+        std::cout << "Evaluate potential at T = " << current_T << " : ";
+        double u = 0;
+        for (size_t n = 0; n <= FH_order; n++){
+            double nfac = 1;
+            for (size_t ni = 2; ni <= n; ni++) nfac *= n;
+            u += pow(D_factors[i][j] / (BOLTZMANN * current_T), n) * T::potential_n(i, j, r, 2 * n) / nfac;
+        }
+        std::cout << r / T::sigma[i][j] << ", " << u / T::eps[i][j] << std::endl;
+        return u;
+    }
+
+    using T::potential_derivative_r;
+    double potential_derivative_r(int i, int j, double r) override {
+        double ur = 0;
+        for (size_t n = 0; n <= FH_order; n++){
+            ur += pow(D_factors[i][j] / (BOLTZMANN * current_T), n) * T::potential_n(i, j, r, 2 * n + 1);
+        }
+        return ur;
+    }
+
+    using T::potential_dblderivative_rr;
+    double potential_dblderivative_rr(int i, int j, double r) override {
+        double urr = 0;
+        for (size_t n = 0; n <= FH_order; n++){
+            urr += pow(D_factors[i][j] / (BOLTZMANN * current_T), n) * T::potential_n(i, j, r, 2 * n + 2);
+        }
+        return urr;
+    }
+
+    size_t set_internals(double rho, double temp, const vector1d& x) override {
+        size_t r = T::set_internals(rho, temp, x);
+        if (temp != current_T){
+            std::cout << "Set current T : " << temp << std::endl;
+            current_T = temp;
+            return r + 1;
+        }
+        return r;
+    }
+
+    void set_masses() override {
+        T::set_masses();
+        set_D_factors();
+    }
+
+    void set_D_factors() {
+        for (size_t i = 0; i < T::Ncomps; i++){
+            for (size_t j = 0; j < T::Ncomps; j++){
+                D_factors[i][j] = pow(HBAR, 2) / (24 * T::red_mass[i][j]);
+            }
+        }
+    }
+
+private:
+    double current_T;
+    vector2d D_factors;
 };
